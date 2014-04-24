95034d73409e93e8245897e0e33c60c7141c1122
Nick Treleaven <nick.treleaven@btinternet.com>
2014-04-24 12:48:51 +0100

    unittest

--- sttrie.d
+++ sttrie.d
@@ -19,21 +19,26 @@ final class Node
     //~ ubyte offset;
     //~ ubyte[] indexes;
     //~ ubyte parentIndex;
+    static collisions = 0;
     
-    bool has(ubyte[] data)
+    this(Table t)
+    {
+        table = t;
+    }
+    
+    bool has(const(ubyte)[] data)
     {
         Node sub = table.nodes[data[0]];
         return sub && sub.parent is this &&
             (data.length == 1 || sub.has(data[1..$]));
     }
     
-    void insert(ubyte[] data)
+    void insert(const(ubyte)[] data)
     {
         Node sub = table.nodes[data[0]];
         if (!sub)
         {
-            sub = new Node;
-            sub.table = table;
+            sub = new Node(table);
             sub.parent = this;
             data.length == 1 || sub.insert(data[1..$]);
             table.nodes[data[0]] = sub;
@@ -42,6 +47,7 @@ final class Node
         if (sub.parent !is this)
         {
             // collision
+            debug collisions++;
             auto t = new Table;
             // TODO opt
             t.nodes = table.nodes;
@@ -52,8 +58,15 @@ final class Node
     }
 }
 
-void main(string[] args)
+unittest
 {
-    auto n = new Node;
+    import std.string : rep = representation;
+    
+    auto n = new Node(new Table);
+    n.insert("hi".rep);
+    assert(n.has("hi".rep));
+    n.insert("ho".rep);
+    assert(n.has("ho".rep));
+    assert(n.has("hi".rep));
 }
 
