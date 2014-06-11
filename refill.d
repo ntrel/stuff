4d24db57747052e6ff4cdc1f80ba3901c994e8af
Nick Treleaven <nick.treleaven@btinternet.com>
2014-06-11 17:33:26 +0100

    Add refill for RA ranges

---
+++ refill.d
@@ -0,0 +1,38 @@
+/* Written in the D programming language.
+ * Copyright: 2014 Nick Treleaven <nick dot treleaven at btinternet com>
+ * License: Boost Software License, Version 1.0. See accompanying file
+ *          LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
+*/
+
+import std.range;
+
+/** Copy up to r.length elements of src to r. */
+auto refill(R, Input)(R r, Input src)
+if (isRandomAccessRange!R && isInputRange!Input)
+{
+    foreach (i, ref e; r)
+    {
+        if (src.empty)
+        {
+            return r[0..i];
+        }
+        e = src.front;
+        src.popFront;
+    }
+    // ignore leftover elements in src
+    return r;
+}
+
+import std.algorithm : uniq;
+/// eager wrapper for uniq
+auto dedup(R)(R r){return r.refill(r.uniq);}
+
+unittest
+{
+    auto a = [5,5,5,5,4,3,3,3,1];
+    auto b = a.dedup;
+    assert (a.ptr is b.ptr);
+    assert(a != b);
+    assert(b == [5, 4, 3, 1]);
+}
+
