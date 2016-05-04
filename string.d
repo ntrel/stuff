936f25680f531cb91c5e418c63d903cce9665844
Nick Treleaven <nick.treleaven@btinternet.com>
2016-05-04 12:02:28 +0100

    Separate SmallString from String, tweaks

--- string.d
+++ string.d
@@ -4,52 +4,102 @@
  *          LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
 */
 
+/* UTF-8 string containers */
+
 @safe:
 
 struct String
 {
-    size_t length;
 private:
+    size_t length;
     /* Note: ptr may point past the end of a memory block, so we must ensure
      * it doesn't escape in @safe code (at least when length == 0). It can
      * escape as void* though for comparisons. */
     immutable(ubyte)* ptr;
     
-    /* Could do Small String Optimization using data + offset in a union with ptr */
-    immutable(ubyte)[size_t.sizeof - 1] data;
-    // needed for popFront
-    ubyte offset;
-    
 public:
     /* Returned `data` slice must not outlive `this`, hence new `return` feature */
     @property immutable(ubyte)[] raw() return @system
     {
-        if (length > size_t.sizeof)
-            return ptr[0..length];
-        else
-            return data[offset..length];
+        return ptr[0..length];
     }
     
-    // Range primitives
+    // Input range primitives
     @property bool empty() {return length == 0;}
     // decodes ptr[0..4]
     @property dchar front();
-    // writes to length, ptr/offset
     void popFront();
+    // Forward range
+    String save() {return this;}
     // BiDi range
+    @property dchar back();
     void popBack();
-    // no opIndex, opSlice because they can break UTF-8
+    // no opIndex, length, opSlice because they can break UTF-8
 }
 
 struct HashString
 {
 immutable:
-    String string;
+    private String str;
     size_t hash;
+    
+    alias str this;
+}
+
+struct SmallString
+{
+private:
+    /* Small String Optimization */
+    enum dataLen = String.sizeof;
+    union
+    {
+        String str;
+        immutable(ubyte)[dataLen] data;
+    }
+    ubyte length;
+    // needed for popFront
+    ubyte offset;
+    
+    @property small() {return length <= dataLen;}
+    
+    @property smallRaw() @system
+    {
+        assert(small);
+        return data[offset..length];
+    }
+
+public:
+    /** Warning: Returned slice must not outlive `this`. */
+    @property immutable(ubyte)[] raw() @system
+    {
+        if (small)
+            return smallRaw;
+        else
+            return str.raw;
+    }
+    
+    String toString() @trusted
+    {
+        if (small)
+        {
+            String s = String(length, smallRaw.idup.ptr);
+            length = ubyte.max;
+            import std.algorithm : move;
+            str = s.move;
+        }
+        return str;
+    }
+
+    // Range primitives
+    @property bool empty() @trusted {return small ? length == 0 : str.empty;}
+    @property dchar front();
+    // writes to str or length, offset
+    void popFront();
 }
 
 unittest
 {
-    
+    SmallString ss;
+    String s = ss.toString;
 }
 
