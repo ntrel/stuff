970dee77cd4071b4a4ae6e5357e54c09edb88e86
Nick Treleaven <nick.treleaven@btinternet.com>
2016-06-21 12:48:07 +0100

    SA: Add opIndex, opEquals, opSlice -> Slice

--- safeSA.d
+++ safeSA.d
@@ -13,8 +13,10 @@ struct StaticArray(SA : T[n], T, size_t n)
 {
     private T[n] data;
 
+    ///
     enum length = n;
 
+    ///
     alias StaticArray_get this;
 
     ref T[n] StaticArray_get() @property @system return
@@ -23,10 +25,69 @@ struct StaticArray(SA : T[n], T, size_t n)
     }
 
     ///
-    void opAssign(ref T[n] sa)
+    void opAssign()(auto ref T[n] sa)
     {
         data = sa;
     }
+
+    ///
+    bool opEquals(T[] s) {return data == s;}
+
+    ///
+    ref opIndex(size_t n) return {return data[n];}
+
+    ///
+    Slice!T opSlice()
+    {
+        return data[].slice;
+    }
+}
+
+/** Immovable, non-copyable slice.
+ * Also useful to force tuple unpacking on foreach(...; Tuple[]) */
+struct Slice(T)
+{
+    private T[] data;
+
+    ///
+    alias Slice_get this;
+
+    ref T[] Slice_get() @property @system return
+    {
+        return data;
+    }
+
+    // TODO: make std.algorithm.move respect this
+    Slice move() @disable;
+    this(this) @disable;
+
+    import std.range.primitives;
+    ///
+    bool empty() @property {return data.empty;}
+    ///
+    auto ref front() @property return {return data.front;}
+    ///
+    void popFront() {data.popFront;}
+
+    ///
+    T[] dup() {return data.dup;}
+    ///
+    immutable(T)[] idup() {return data.idup;}
+
+    ///
+    void opAssign(T[] s)
+    {
+        data[] = s;
+    }
+
+    ///
+    bool opEquals(T[] s) {return data == s;}
+}
+
+/// ditto
+Slice!T slice(T)(T[] s)
+{
+    return Slice!T(s);
 }
 
 ///
@@ -34,22 +95,34 @@ struct StaticArray(SA : T[n], T, size_t n)
 {
     alias SA = StaticArray!(int[2]);
     auto sa = SA();
-    int[2] arr;
+    int[2] arr = [1, 2];
 
     static assert(sa.length == 2);
     static assert(!__traits(compiles, arr = sa));
     static assert(!__traits(compiles, arr = SA()));
-    static assert(!__traits(compiles, sa[]));
     static assert(!__traits(compiles, sa.ptr));
+    static assert(!__traits(compiles, {int[] s = sa[];}));
+
+    assert(sa == [0, 0]);
+    sa = arr;
+    assert(sa == [1, 2]);
+    arr[0] = 3;
+    sa[] = arr; // implicit slicing of int[2]
+    assert(sa[0] == 3);
+    sa[1] = 4;
+    assert(sa == [3, 4]);
+    sa[] = [5, 6];
+    assert(sa[] == [5, 6]);
 
     ()@trusted{
+        // implicit conversion to int[2] in @system code
         arr = SA();
-        int[] invalid = SA();
-        int[] s2 = sa[];
+        int[] invalid = SA(); // due to compiler allowing implicit slicing of rvalue int[2]
         int* p = sa.ptr;
-        assert(p is sa.data.ptr);
+        assert(p is sa[].ptr);
+        // Slice!int converts to int[] in @system code:
+        int[] s = sa[];
+        s = sa; // implicit slicing of int[2]
     }();
-
-    sa = arr;
 }
 
