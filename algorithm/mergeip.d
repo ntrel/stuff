0e8e6f2c4346f36d2d062ad583107638b306bd32
Nick Treleaven <nick.treleaven@btinternet.com>
2016-07-21 11:52:32 +0100

    Merge in place: Test with std.random.uniform
    add attributes, move to algorithm/.

--- algorithm/mergeip.d
+++ algorithm/mergeip.d
@@ -4,24 +4,27 @@
  *          LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.
 */
 
+/// Merge in-place without allocating
+// FIXME
 
-import std.algorithm;
+
+import std.algorithm : find, min, swap;
 import std.traits : isSomeChar;
-debug import std.stdio;
+debug import std.stdio : writeln, writefln;
 
-void merge(T)(T[] left, T[] right)
+void merge(T)(T[] left, T[] right) @nogc
     if (!isSomeChar!T)
 {
     if (!right.length)
         return;
     debug writeln(left, right);
-    
+
     // skip in place elements
     const r = right[0];
     left = left.find!(e => e > r);
     if (!left.length)
         return;
-    
+
     // swap runs of elements
     const len = min(left.length, right.length);
     size_t i;
@@ -30,11 +33,11 @@ void merge(T)(T[] left, T[] right)
         swap(left[i], right[i]);
         i++;
     } while (i != len && left[i] > right[i]);
-    
+
     // number of elements swapped
     const n = i;
     debug writefln(">%s%s; %s", left, right, n);
-    
+
     // may need to re-merge right if we ran out of left elements to swap
     if (n == left.length)
         merge(right[0..n], right[n..$]);
@@ -42,10 +45,11 @@ void merge(T)(T[] left, T[] right)
         merge(left[n..$], right);
 }
 
-unittest
+@safe unittest
 {
     void test(T)(T l, T r)
     {
+        import std.algorithm : sort;
         const s = sort(l ~ r).release;
         merge(l, r);
         assert(l ~ r == s);
@@ -58,3 +62,19 @@ unittest
     test([4,5,6], [1,2]);
 }
 
+@safe unittest
+{
+    import std.random : uniform;
+
+    //~ auto a = new int[uniform(100, 200)];
+    auto a = new int[uniform(10, 20)];
+    foreach (ref e; a)
+    {
+        e = uniform(-100, 100);
+    }
+    import std.algorithm : sort;
+    const sa = sort(a.dup).release;
+    merge(a[0..$/2].sort().release, a[$/2..$].sort().release);
+import std.stdio; a.writeln; sa.writeln;
+    assert(a == sa);
+}
