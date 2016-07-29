2bbdb8d515298bc54696440db8801cef0c786e32
Nick Treleaven <nick.treleaven@btinternet.com>
2016-07-29 11:54:13 +0100

    mergeip: Attempt proper implementation

--- algorithm/mergeip.d
+++ algorithm/mergeip.d
@@ -5,15 +5,15 @@
 */
 
 /// Merge in-place without allocating
-// FIXME
 
 
 import std.algorithm : find, min, swap;
-import std.traits : isSomeChar;
+import std.range.primitives;
 debug import std.stdio : writeln, writefln;
 
+version(Wrong)
 void merge(T)(T[] left, T[] right) @nogc
-    if (!isSomeChar!T)
+    if (hasAssignableElements!(T[]))
 {
     if (!right.length)
         return;
@@ -45,6 +45,47 @@ void merge(T)(T[] left, T[] right) @nogc
         merge(left[n..$], right);
 }
 
+// FIXME
+void merge(T)(T[] left, T[] right) //@nogc
+    if (hasAssignableElements!(T[]))
+{
+    if (left.empty || right.empty) return;
+
+    // merge elements into left
+    size_t li, ri;
+outer:
+    while (1)
+    {
+import std.stdio; writeln(left, right);
+        if (left[li] > right[ri])
+        {
+            swap(left[li], right[ri]);
+            ri++;
+            if (ri == right.length) return;
+        }
+writeln(left, right);
+        li++;
+        if (li == left.length) break;
+        // ensure right[0] > left[li] and keep swapped elements in order
+        size_t ti;
+        while (ti < ri && left[li] > right[ti])
+        {
+            swap(left[li], right[ti]);
+writeln(left[li..$], right[0..ri], "-shift");
+            li++;
+            if (li == left.length)
+            {
+                merge(right[0..ti], right[ti..ri]);
+                break outer;
+            }
+            ti++;
+        }
+    }
+    // merge swapped elements
+    merge(right[0..ri], right[ri..$]);
+}
+
+version(Disabled)
 @safe unittest
 {
     void test(T)(T l, T r)
@@ -67,14 +108,16 @@ void merge(T)(T[] left, T[] right) @nogc
     import std.random : uniform;
 
     //~ auto a = new int[uniform(100, 200)];
-    auto a = new int[uniform(10, 20)];
+    auto a = new int[uniform(8, 10)];
     foreach (ref e; a)
     {
-        e = uniform(-100, 100);
+        //~ e = uniform(-100, 100);
+        e = uniform(0, 100);
     }
     import std.algorithm : sort;
     const sa = sort(a.dup).release;
-    merge(a[0..$/2].sort().release, a[$/2..$].sort().release);
-import std.stdio; a.writeln; sa.writeln;
+    const n = a.length / 2;
+    merge(a[0..n].sort().release, a[n..$].sort().release);
+import std.stdio; writeln(a); writeln(sa);
     assert(a == sa);
 }
