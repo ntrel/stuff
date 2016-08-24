9699716eb04a20f2d87aeac1e9c53382e23912c9
Nick Treleaven <n@trelsoft.com>
2016-08-24 16:55:07 +0100

    Add RefCountedSlice from DIP1000

---
+++ typecons/rcs.d
@@ -0,0 +1,56 @@
+@safe struct RefCountedSlice(T) {
+	private T[] payload;
+	private uint* count;
+
+	this(size_t initialSize) {
+		payload = new T[initialSize];
+		count = new size_t;
+		*count = 1;
+	}
+
+	this(this) {
+		if (count) ++*count;
+	}
+
+	// Prevent reassignment as references to payload may still exist
+	@system opAssign(RefCountedSlice rhs) {
+		this.__dtor();
+		payload = rhs.payload;
+		count = rhs.count;
+		++*count;
+	}
+
+    // Interesting fact #1: destructor can be @trusted
+	@trusted ~this() {
+		if (count && !--*count) {
+			delete payload;
+			delete count;
+		}
+	}
+
+    // Interesting fact #2: references to internals can be given away
+	//~ scope
+	ref T opIndex(size_t i) {
+		return payload[i];
+	}
+
+	// ...
+}
+
+// Prevent premature destruction as references to payload may still exist
+// (defined in object.d)
+@system void destroy(T)(ref RefCountedSlice!T rcs);
+
+@safe unittest
+{
+    alias RCS = RefCountedSlice!int;
+    static fun(ref RCS rc, ref int ri)
+    {
+        rc = rc.init; // Error: can't call opAssign in @safe code
+        rc.destroy;   // Error: can't call destroy(RCS) in @safe code
+        ri++;         // would have been unsafe
+    }
+    auto rc = RCS(1);
+    fun(rc, rc[0]);
+    assert(rc[0] == 1);
+}
