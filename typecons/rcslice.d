2d68a410edba319678788d18ef1412797237cdb6
Nick Treleaven <n@trelsoft.com>
2016-09-03 15:29:15 +0100

    Use RCSlice.data pointer for efficiency, offset to find memory

--- typecons/rcslice.d
+++ typecons/rcslice.d
@@ -1,31 +1,35 @@
 
 /** Parameters:
  * Length=uint saves register space but limits allocation size */
-struct RCSlice(T, Length=uint)
+@safe struct RCSlice(T, Length=uint)
 {
 private:
-	Length offset;
+	T* data;
+	Length offset;	// mem = data - offset
 	Length length;
-	Impl* impl;
 
 	struct Impl
 	{
 	private:
-		/* Stored before *impl.
-		 * Last byte determines preceding length size in bytes:
-		 * nBytes = lengthBytes & 7 + 1 */
-		ubyte[0] lengthBytes;
 		uint refCount;
-		Length capacity;
 		T[0] data;
 	}
 
-	this(Length size)
+	this(Length size) @trusted
 	{
-		const lbSize = 1 + 0;
-		const n = lbSize + Impl.sizeof + T.sizeof * size;
-		auto mem = new ubyte[n].ptr;
-		impl = cast(Impl*)mem + lbSize;
-		//static if (hasIndirections!T) GC.addRange
+		const n = Impl.sizeof + T.sizeof * size;
+		void[] mem = new ubyte[n];
+		import core.memory, std.traits;
+		static if (hasIndirections!T)
+			GC.addRange(mem);
+
+		auto impl = cast(Impl*)mem.ptr;
+		impl.refCount = 1;
+		data = impl.data.ptr;
 	}
+}
+
+unittest
+{
+	RCSlice!int rcs;
 }
