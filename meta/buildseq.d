06cc59e8ececeee351e6df6c11bd63b4724b1c4c
Nick Treleaven <n@trelsoft.com>
2020-06-08 13:19:46 +0100

    Add NoDuplicates, ReplaceAll

--- meta/buildseq.d
+++ meta/buildseq.d
@@ -14,6 +14,7 @@ alias Map(alias Tem, S...) =
 alias Filter(alias pred, S...) =
 	__buildSeq(E, S, pred!E ? E : AliasSeq!());
 
+// in case ternary can't work, but then we instantiate O(n) of Select!
 import std.traits : Select;
 alias Filter(alias pred, S...) =
 	__buildSeq(E, S, Select!(pred!E, E, AliasSeq!()));
@@ -23,18 +24,25 @@ alias Filter(alias pred, S...) =
 alias EraseAll(alias Item, S...) =
 	__buildSeq(E, S, Select!(__traits(isSame, E, Item), AliasSeq!(), E));
 
+// search for each element in the sequence constructed so far, __Result
+alias NoDuplicates(S...) =
+	__buildSeq(E, S, (staticIndexOf!(E, __Result) == -1) ? E : AliasSeq!());
+
 alias Repeat(size_t n, S...) =
 	__buildSeq(i, 0..n, S);
+
+alias ReplaceAll(alias T, alias U, S...) =
+	__buildSeq(E, S, __traits(isSame, E, T) ? U : E);
 
 template Stride(int stepSize, S...)
 if (stepSize != 0)
 {
 	static if (stepSize > 0)
-		alias Stride = __buildSeq(E, S, Select!(i % stepSize == 0, S[i], AliasSeq!()));
+		alias Stride = __buildSeq(E, S, (i % stepSize == 0) ? S[i] : AliasSeq!());
 	else
-		alias Stride = __buildSeq(E, S, Select!(i % stepSize == 0, S[$ - 1 - i], AliasSeq!()));
+		alias Stride = __buildSeq(E, S, (i % stepSize == 0) ? S[$ - 1 - i] : AliasSeq!());
 }
 
-// don't use recursion:
+// these don't use recursion:
 // aliasSeqOf, allSatisfy, anySatisfy, staticIndexOf
-// Erase shouldn't use recursion
+// Erase, Replace shouldn't use recursion - use staticIndexOf
