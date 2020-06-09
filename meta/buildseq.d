39a5fd85ec3eddfa2186fdfad9b3af54a34f0544
Nick Treleaven <n@trelsoft.com>
2020-06-09 12:15:55 +0100

    __buildSeq -> static foreach

--- meta/buildseq.d
+++ meta/buildseq.d
@@ -1,46 +1,46 @@
-SequenceBuilder
-	__buildSeq(Identifier, LwrExpression .. UprExpression, Expression)
-	__buildSeq(Identifier, SeqExpression, Expression)
+StaticForeachExpr
+	static foreach(Identifier; LwrExpression .. UprExpression; Expression)
+	static foreach(Identifier; SeqExpression; Expression)
 
 alias staticIota(size_t start, size_t end) =
-	__buildSeq(i, start..end, i);
+	static foreach(i; start..end; i);
 
-__buildSeq(i, 0..3, staticIota!(0, i))
+static foreach(i; 0..3; staticIota!(0, i))
 // (0, 0, 1, 0, 1, 2)
 
 alias Map(alias Tem, S...) =
-	__buildSeq(E, S, Tem!E);
+	static foreach(E; S; Tem!E);
 
 alias Filter(alias pred, S...) =
-	__buildSeq(E, S, pred!E ? E : AliasSeq!());
+	static foreach(E; S; pred!E ? E : AliasSeq!());
 
 // in case ternary can't work, but then we instantiate O(n) of Select!
 import std.traits : Select;
 alias Filter(alias pred, S...) =
-	__buildSeq(E, S, Select!(pred!E, E, AliasSeq!()));
+	static foreach(E; S; Select!(pred!E, E, AliasSeq!()));
 
 /// remainder of std.meta:
 
 alias EraseAll(alias Item, S...) =
-	__buildSeq(E, S, Select!(__traits(isSame, E, Item), AliasSeq!(), E));
+	static foreach(E; S; Select!(__traits(isSame, E, Item), AliasSeq!(), E));
 
-// search for each element in the sequence constructed so far, __Result
+// search for each element in the sequence constructed so far: __Result
 alias NoDuplicates(S...) =
-	__buildSeq(E, S, (staticIndexOf!(E, __Result) == -1) ? E : AliasSeq!());
+	static foreach(E; S; (staticIndexOf!(E, __Result) == -1) ? E : AliasSeq!());
 
 alias Repeat(size_t n, S...) =
-	__buildSeq(i, 0..n, S);
+	static foreach(i; 0..n; S);
 
 alias ReplaceAll(alias T, alias U, S...) =
-	__buildSeq(E, S, __traits(isSame, E, T) ? U : E);
+	static foreach(E; S; __traits(isSame, E, T) ? U : E);
 
 template Stride(int stepSize, S...)
 if (stepSize != 0)
 {
 	static if (stepSize > 0)
-		alias Stride = __buildSeq(E, S, (i % stepSize == 0) ? S[i] : AliasSeq!());
+		alias Stride = static foreach(E; S; (i % stepSize == 0) ? S[i] : AliasSeq!());
 	else
-		alias Stride = __buildSeq(E, S, (i % stepSize == 0) ? S[$ - 1 - i] : AliasSeq!());
+		alias Stride = static foreach(E; S; (i % stepSize == 0) ? S[$ - 1 - i] : AliasSeq!());
 }
 
 // these don't use recursion:
