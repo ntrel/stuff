965fec58ed064e495b9d9abf69f45c8a1daf0b07
Nick Treleaven <n@trelsoft.com>
2020-06-10 17:15:52 +0100

    Add notes on different __Fold forms

--- meta/fold.d
+++ meta/fold.d
@@ -19,6 +19,8 @@ alias NoDuplicates(S...) =
     __Fold(Acc...; foreach(E; S) =>
         AliasSeq!(Acc, AliasSeq!E[0..staticIndexOf!(E, Acc) == -1]));
 
+/// we can't implement short-circuit staticIndexOf just with foreach
+
 FoldLoopDecl~=
     FoldLoopDecl while (Expression)
 
@@ -26,6 +28,8 @@ FoldLoopDecl~=
 enum staticIndexOf(alias A, S...) =
     __Fold!(size_t acc = -1; foreach(i, E; S) while (acc == -1)
         => [-1, i][isSame!(A, E)];
+
+/// we can't implement std.traits.fullyQualifiedName with foreach
 
 // this form is hard to read, see the next grammar variant for better syntax
 // this form allows the NextExpression to be e.g. int[2] rather than an AliasSeq
@@ -42,19 +46,21 @@ enum fqn(alias A) =
     __Fold(Acc... = AliasSeq!(A, ""); while (!__traits(isSame, Acc[0], null))
         => AliasSeq!(__traits(parent, Acc[0]), Acc[1] ~ Acc[0].stringof))[1];
 
+/// this form allows separate naming of each component of the accumulator above
 AccumulatorDecl~=
     AccumulatorDecl; AccumulatorDecl
 
 // NextExpression must be a sequence whose length must correspond to the AccumulatorDecls
 // Only one AccumulatorDecl can be a sequence
 enum staticIndexOf(alias A, S...) =
-    __Fold!(int acc = -1; int i = 0; while (acc == -1 && i != S.length)
+    __Fold!(int acc = -1; uint i = 0; while (acc == -1 && i != S.length)
         => AliasSeq!([-1, i][isSame!(A, S[i])], i + 1))[0];
 
 enum fqn(alias A) =
     __Fold(string acc = ""; alias P = A; while (!__traits(isSame, P, null))
         => AliasSeq!(acc ~ P.stringof, __traits(parent, P)))[0];
 
+/// this form separates the loop logic from the NextExpression
 FoldLoopDecl~=
     for(Declaration; Expression; AssignExpression)
 
