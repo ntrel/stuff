c9aa1edce2089589f53c0d1e862de3f0cdc8fac8
Nick Treleaven <ntrel002@gmail.com>
2023-01-28 11:05:56 +0000

    Make assumeIsolated auto ref and null the param

--- typecons/isolated.d
+++ typecons/isolated.d
@@ -1,18 +1,21 @@
 @safe:
 
-@system assumeIsolated(T)(T v)
+@system assumeIsolated(T)(auto ref T v)
 {
-    return Isolated!T(v);
+    auto r = Isolated!T(v);
+    static if (__traits(compiles, v = null))
+        v = null;
+    return r;
 }
 
 struct Isolated(T)
 {
     private T data;
-    
+
     private this(T v) { data = v; }
-    
+
     @disable this(this);
-    
+
     T unwrap()
     {
         auto d = data;
@@ -30,12 +33,12 @@ interface IAllocator
 class Mallocator : IAllocator
 {
     import core.stdc.stdlib : free, malloc;
-    
+
     void* allocate(size_t n) @trusted
     {
         return malloc(n);
     }
-    
+
     void safeDeallocate(ref Isolated!(void*) ip) @trusted
     {
         ip.unwrap.free;
@@ -45,7 +48,8 @@ class Mallocator : IAllocator
 void main()
 {
     IAllocator a = new Mallocator;
-    auto ip = (() @trusted => assumeIsolated(a.allocate(4)))();
+    scope m = a.allocate(4);
+    auto ip = (() @trusted => assumeIsolated(m))();
     a.safeDeallocate(ip);
     assert(ip.unwrap == null);
 }
